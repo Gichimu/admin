f9a6919680fd8122f7c0d5762a692dbfcc167871
Gichimu <gichimueric12@gmail.com>
2022-07-20 09:46:31 +0300

    add dashboard items

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Kid } from '../kid';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  kids: Kid;
+  ages: number[] = [];
 
-  constructor() { }
+  constructor(private readonly httpService: HttpService) {}
 
   ngOnInit(): void {
-    
+    this.httpService
+      .getKids()
+      .subscribe((kids) => {
+        this.kids = kids;
+      });
   }
 
+  capitalizeFirstLetter(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
 }
